a129fa19d17755ea734dd5d666e51e141479cc47
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-06 11:51:45 +0200

    Add tilt ball switch

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,24 @@
 // www.kuongshun.com
 // 2023.6.18
 
-#include <pitches.h>
-
-// notes in the melody:
-int melody[] = {
-    NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-int duration = 500; // 500 miliseconds
+const int ledPin = 13; // the led attach to
 
 void setup()
 {
+  pinMode(ledPin, OUTPUT); // initialize the ledPin as an output
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);
 }
 
 void loop()
 {
-  for (int thisNote = 0; thisNote < 8; thisNote++)
+  int digitalVal = digitalRead(2); // read pin 2
+  if (HIGH == digitalVal)
   {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(8, melody[thisNote], duration);
-
-    // Output the voice after several minutes
-    delay(1000);
+    digitalWrite(ledPin, LOW); // turn the led off
   }
-
-  // restart after two seconds
-  delay(2000);
+  else
+  {
+    digitalWrite(ledPin, HIGH); // turn the led on
+  }
 }
