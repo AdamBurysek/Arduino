3409aa46fd320894aa302bd29370c87cda8d8e65
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-29 16:53:29 +0200

    Add therometer

--- projects/lcd_thermometer.cpp
+++ projects/lcd_thermometer.cpp
@@ -1,5 +1,5 @@
-// www.kuongshun.com
-// 2023.6.18
+//www.kuongshun.com
+//2023.6.18
 
 #include <Arduino.h>
 
@@ -7,40 +7,37 @@
 int tempPin = 0;
 //                BS  E  D4 D5  D6 D7
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
 void setup()
 {
   /*lcd.begin(cols, rows)
     lcd: a variable of type LiquidCrystal
     cols: the number of columns that the display has
-    rows: the number of rows that the display has
+    rows: the number of rows that the display has 
   */
   lcd.begin(16, 2);
 }
-
 void loop()
 {
   int tempReading = analogRead(tempPin);
   // This is OK
   double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK); //  Temp Kelvin
-  float tempC = tempK - 273.15;                                                          // Convert Kelvin to Celcius
-  float tempF = (tempC * 9.0) / 5.0 + 32.0;                                              // Convert Celcius to Fahrenheit
+  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
+  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
+  float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
   /*  replaced
     float tempVolts = tempReading * 5.0 / 1024.0;
     float tempC = (tempVolts - 0.5) * 10.0;
     float tempF = tempC * 9.0 / 5.0 + 32.0;
   */
-  float tempWithCorrection = tempC + 1.4;
   // Display Temperature in C
   lcd.setCursor(0, 0);
   lcd.print("Temp         C  ");
   // Display Temperature in F
-  // lcd.print("Temp         F  ");
+  //lcd.print("Temp         F  ");
   lcd.setCursor(6, 0);
   // Display Temperature in C
-  lcd.print(tempWithCorrection);
+  lcd.print(tempC);
   // Display Temperature in F
-  // lcd.print(tempF);
+  //lcd.print(tempF);
   delay(500);
 }
