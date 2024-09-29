b0cf22be01c6c69a95d27735c3bbfda537ca4a90
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-29 11:28:09 +0200

    Add LCD display

--- lib/LiquidCrystal/examples/HelloWorld/HelloWorld.ino
+++ lib/LiquidCrystal/examples/HelloWorld/HelloWorld.ino
@@ -1,12 +1,7 @@
-//www.kuongshun.com
-//2023.6.18
-
-#include <Arduino.h>
-
 /*
   LiquidCrystal Library - Hello World
 
- Demonstrates the use of a 16x2 LCD display.  The LiquidCrystal
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
  library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
@@ -15,12 +10,12 @@
  and shows the time.
 
   The circuit:
- * LCD RS pin to digital pin 7
- * LCD Enable pin to digital pin 8
- * LCD D4 pin to digital pin 9
- * LCD D5 pin to digital pin 10
- * LCD D6 pin to digital pin 11
- * LCD D7 pin to digital pin 12
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
  * LCD R/W pin to ground
  * LCD VSS pin to ground
  * LCD VCC pin to 5V
@@ -30,11 +25,11 @@
 
  Library originally added 18 Apr 2008
  by David A. Mellis
- Library modified 5 Jul 2009
+ library modified 5 Jul 2009
  by Limor Fried (http://www.ladyada.net)
- Example added 9 Jul 2009
+ example added 9 Jul 2009
  by Tom Igoe
- Modified 22 Nov 2010
+ modified 22 Nov 2010
  by Tom Igoe
 
  This example code is in the public domain.
@@ -46,19 +41,20 @@
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Hello, World!");
+  lcd.print("hello, world!");
 }
 
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(5, 1);
+  lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
 }
+
